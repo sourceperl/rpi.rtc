1972cf8b02473ca9b96e66af47415ea5be3bcedf
sourceperl <loic.celine@free.fr>
2018-03-27 10:58:31 +0200

    add explicit error message for RTC communication fail

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyRPiRTC',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     url='https://github.com/sourceperl/rpi.rtc',
     platforms='any',
